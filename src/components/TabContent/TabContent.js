ed2206c617af417539a0b6fae7b5342d95270d09
George <georgeak752@gmail.com>
2024-01-18 17:50:08 +0200

    1st

---
+++ src/components/TabContent/TabContent.js
@@ -0,0 +1,53 @@
+import { useState } from "react";
+
+export default function TabContent({ item }) {
+  const [showDetails, setShowDetails] = useState(true);
+  const [likes, setLikes] = useState(0);
+
+  console.log("RENDER");
+
+  function handleInc() {
+    setLikes(likes + 1);
+  }
+
+  function handleTripleInc() {
+    setLikes(likes + 3);
+    // or
+    // setLikes((likes) => likes + 1);
+    // setLikes((likes) => likes + 1);
+    // setLikes((likes) => likes + 1);
+  }
+
+  function handleUndo() {
+    setShowDetails(true);
+    setLikes(0);
+  }
+
+  function handleUndoLater() {
+    setTimeout(handleUndo, 2000);
+  }
+
+  return (
+    <div className="tab-content">
+      <h4>{item.summary}</h4>
+      {showDetails && <p>{item.details}</p>}
+
+      <div className="tab-actions">
+        <button onClick={() => setShowDetails((h) => !h)}>
+          {showDetails ? "Hide" : "Show"} details
+        </button>
+
+        <div className="hearts-counter">
+          <span>{likes} ❤️</span>
+          <button onClick={handleInc}>+</button>
+          <button onClick={handleTripleInc}>+++</button>
+        </div>
+      </div>
+
+      <div className="tab-undo">
+        <button onClick={handleUndo}>Undo</button>
+        <button onClick={handleUndoLater}>Undo in 2s</button>
+      </div>
+    </div>
+  );
+}
